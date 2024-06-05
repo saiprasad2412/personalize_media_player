483e8e6c27abfa128d9e5ca8a9ffe742b38edbfa
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-06-05 18:44:30 +0530

    controller error sollve

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,19 @@
-const asyncHandler=(fn)=> async (req,res,next)=>{
-    try {
-        await fn(req,res,next)
+// const asyncHandler=(fn)=> async (req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
         
-    } catch (error) {
-        res.status(err.code||500).json({message:error.message})
+//     } catch (error) {
+//         res.status(error.code||500).json({message:error.message})
+//     }
+// }
+
+// export {asyncHandler}
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export {asyncHandler}
+
+export { asyncHandler }
